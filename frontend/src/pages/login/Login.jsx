4171a3527f5bafd913d7f832d11987b82999e76c
Ritik Gupta <ritikgupta2602@gmail.com>
2024-08-13 17:54:10 +0530

    frontend completed

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          Login<span className="text-blue-500"> Chat</span>
+        </h1>
+
+        <form>
+          <div>
+            <label className="label">
+              <span className="text-base label-text">User Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="User Name"
+              className="input input-bordered w-full h-10"
+            />
+          </div>
+          <label className="label">
+            <span className="text-base label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            placeholder="Password"
+            className="input input-bordered w-full h-10"
+          />
+          <div>
+            <a
+              href="#"
+              className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+            >
+              {"Don't"} Have an account
+            </a>
+
+            <div>
+              <button className="btn btn-block btn-sm mt-2">Login</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    // <div>Login</div>
+  );
+};
+
+export default Login;
---
+++ frontend/src/pages/signup/SignUp.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Gender from "./Gender";
+
+const SignUp = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          SignUp<span className="text-blue-500"> Chat</span>
+        </h1>
+
+        <form>
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Full Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="input input-bordered w-full h-10"
+            />
+          </div>
+
+          <div>
+            <label className="label">
+              <span className="text-base label-text">User Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="User Name"
+              className="input input-bordered w-full h-10"
+            />
+          </div>
+
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Password"
+              className="input input-bordered w-full h-10"
+            />
+          </div>
+
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Confirm Password</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Confirm Password"
+              className="input input-bordered w-full h-10"
+            />
+          </div>
+
+          <Gender/>
+
+          <a
+              href="#"
+              className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+            >
+              Already have an account
+            </a>
+
+            <div>
+              <button className="btn btn-block btn-sm mt-2">SignUp</button>
+            </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
