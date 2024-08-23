f2d92094e54b6d167f37096d9404fd4260c1c35d
Ritik Gupta <ritikgupta2602@gmail.com>
2024-08-24 00:12:29 +0530

    try to fix cors

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://chat-app-wh8v.onrender.com/"],
+    origin: ["https://chat-app-wh8v.onrender.com"],
     methods: ["GET", "POST"],
   },
 });
